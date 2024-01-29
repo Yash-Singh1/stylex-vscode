e59e9fbf7e9bc2c040ad663e1d75f922944d265d
Yash Singh <saiansh2525@gmail.com>
2024-01-28 17:52:32 -0800

    chore: move server compile to server dir

--- server/tsup.config.ts
+++ server/tsup.config.ts
@@ -6,5 +6,11 @@ export default defineConfig({
       js: `.${format}`,
     };
   },
+  splitting: false,
+  outDir: "out",
   banner: { js: "//shaylovestypescriptggs server code start" },
+  clean: true,
+  entry: ["src/server.ts"],
+  bundle: true,
+  external: ["prettier"],
 });
