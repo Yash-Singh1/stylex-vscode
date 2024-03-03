458942e333e62919e827f2c428e5c08cdf0af051
Yash Singh <saiansh2525@gmail.com>
2024-03-02 19:16:21 -0800

    chore: use keyList instead of keys for lru cache

--- server/src/lib/lru-cache.ts
+++ server/src/lib/lru-cache.ts
@@ -1,27 +1,27 @@
-// Based off https://github.com/trekhleb/javascript-algorithms/blob/master/src/data-structures/lru-cache/LRUCacheOnMap.js
-
 class LRUCache<K, V> extends Map<K, V> {
   private capacity: number;
+  private keyList: K[];
 
   constructor(capacity: number) {
     super();
     this.capacity = capacity;
+    this.keyList = [];
   }
 
   get(key: K) {
     if (!super.has(key)) return undefined;
-    const val = super.get(key)!;
-    super.delete(key);
-    super.set(key, val);
-    return val;
+    this.keyList = [...this.keyList.filter((k) => k !== key), key];
+    return super.get(key);
   }
 
   set(key: K, val: V) {
     super.delete(key);
     super.set(key, val);
 
-    if (super.size > this.capacity) {
-      super.delete(super.keys().return!().value as K);
+    this.keyList = [...this.keyList.filter((k) => k !== key), key];
+    if (this.keyList.length > this.capacity) {
+      super.delete(this.keyList[0]);
+      this.keyList.shift();
     }
 
     return this;
